ad2430639b4ec18f16c324823839ab78232fcbd5
Klim Huynh <klimhuynh@gmail.com>
2023-10-17 15:16:52 +1100

    Add DebtRecord class

---
+++ src/main/java/com/mnfll/bill_splitter_app/DebtRecord.java
@@ -0,0 +1,37 @@
+package com.mnfll.bill_splitter_app;
+
+public class DebtRecord {
+	private String creditor;
+	private String debtor;
+	private double amountOwed;
+	
+	public DebtRecord(String creditor, String debtor, double amountOwed) {
+		this.creditor = creditor;
+		this.debtor = debtor;
+		this.amountOwed = amountOwed;
+	}
+
+	public String getCreditor() {
+		return creditor;
+	}
+
+	public void setCreditor(String creditor) {
+		this.creditor = creditor;
+	}
+
+	public String getDebtor() {
+		return debtor;
+	}
+
+	public void setDebtor(String debtor) {
+		this.debtor = debtor;
+	}
+
+	public double getAmountOwed() {
+		return amountOwed;
+	}
+
+	public void setAmountOwed(double amountOwed) {
+		this.amountOwed = amountOwed;
+	}
+}
