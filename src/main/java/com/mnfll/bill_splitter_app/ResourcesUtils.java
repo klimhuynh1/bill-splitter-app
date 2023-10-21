282be10c76442f765ba965e9575b4d1ab109ff76
Klim Huynh <klimhuynh@gmail.com>
2023-10-21 14:42:15 +1100

    Update JUnit and Maven Compiler Plugin versions and add TestNG for Maven Project

---
+++ src/main/java/com/mnfll/bill_splitter_app/ResourcesUtils.java
@@ -0,0 +1,44 @@
+package com.mnfll.bill_splitter_app;
+
+import java.sql.*;
+
+public class ResourcesUtils {
+    public static void closeResult(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeStatement(Statement stmt) {
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closePrepapredStatement(PreparedStatement pstmt) {
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeConnection(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+}
