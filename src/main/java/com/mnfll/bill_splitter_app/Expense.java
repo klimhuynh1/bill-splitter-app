5340ea1684d25729ba1eb06772a6580e16032571
Klim Huynh <klimhuynh@gmail.com>
2024-07-25 17:25:38 +1000

    Reformat code for consistency

--- src/main/java/com/mnfll/bill_splitter_app/Expense.java
+++ src/main/java/com/mnfll/bill_splitter_app/Expense.java
@@ -4,65 +4,60 @@ import java.util.Date;
 import java.util.List;
 
 public class Expense {
-	private final Date date;
-	private final String establishmentName;
-	private final String itemName;
-	private final double itemCost;
-	private final List<String> debtorNames;
-	private final String creditorName;
-	
-	public Expense(Date date, String establishmentName, String itemName, double itemCost, List<String> debtorNames,
-			String creditorName) {
-		super();
-		this.date = date;
-		this.establishmentName = establishmentName;
-		this.itemName = itemName;
-		this.itemCost = itemCost;
-		this.debtorNames = debtorNames;
-		this.creditorName = creditorName;
-	}
+    private final Date date;
+    private final String establishmentName;
+    private final String itemName;
+    private final double itemCost;
+    private final List<String> debtorNames;
+    private final String creditorName;
 
-	public Date getDate() {
-		return date;
-	}
+    public Expense(Date date, String establishmentName, String itemName, double itemCost, List<String> debtorNames,
+                   String creditorName) {
+        super();
+        this.date = date;
+        this.establishmentName = establishmentName;
+        this.itemName = itemName;
+        this.itemCost = itemCost;
+        this.debtorNames = debtorNames;
+        this.creditorName = creditorName;
+    }
 
-
-
-	public String getEstablishmentName() {
-		return establishmentName;
-	}
+    public Date getDate() {
+        return date;
+    }
 
 
-
-	public String getItemName() {
-		return itemName;
-	}
-
+    public String getEstablishmentName() {
+        return establishmentName;
+    }
 
 
-	public double getItemCost() {
-		return itemCost;
-	}
+    public String getItemName() {
+        return itemName;
+    }
 
 
-
-	public List<String> getDebtorNames() {
-		return debtorNames;
-	}
-
+    public double getItemCost() {
+        return itemCost;
+    }
 
 
-	public String getCreditorName() {
-		return creditorName;
-	}
+    public List<String> getDebtorNames() {
+        return debtorNames;
+    }
 
-	public void displayExpense() {
-		System.out.println("Date: " + getDate());
-		System.out.println("Establishment: " + getEstablishmentName());
-		System.out.println("Item: " + getItemName());
-		System.out.println("Cost: " + getItemCost());
-		System.out.println("Debtor Names " + getDebtorNames());
-		System.out.println("Creditor Name: " + getCreditorName());
-		System.out.println();
-	}
+
+    public String getCreditorName() {
+        return creditorName;
+    }
+
+    public void displayExpense() {
+        System.out.println("Date: " + getDate());
+        System.out.println("Establishment: " + getEstablishmentName());
+        System.out.println("Item: " + getItemName());
+        System.out.println("Cost: " + getItemCost());
+        System.out.println("Debtor Names " + getDebtorNames());
+        System.out.println("Creditor Name: " + getCreditorName());
+        System.out.println();
+    }
 }
