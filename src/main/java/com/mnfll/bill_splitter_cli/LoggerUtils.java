3c42652e585f69e5d8df87c30ea7933e2cdc3b81
Klim Huynh <klimhuynh@gmail.com>
2024-07-28 17:43:47 +1000

    chore: remove LoggerUtils class

--- src/main/java/com/mnfll/bill_splitter_cli/LoggerUtils.java
+++
@@ -1,8 +0,0 @@
-package com.mnfll.bill_splitter_cli;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class LoggerUtils {
-    protected static final Logger logger = LogManager.getLogger();
-}
